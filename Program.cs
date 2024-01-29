519379fa819afc1da49644e9470606c198d03bd1
Asia <asiarie99@gmail.com>
2024-01-29 19:16:59 +0100

    Wait for Enter tofinish the game

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Let's play Rock-Paper-Scissors"!);
+Console.WriteLine("Let's play Rock-Paper-Scissors!");
+Console.WriteLine("Press Enter to close the game...");
+Console.ReadLine();
