4e5b2ecb7d56fbe2fc9bd4c83ea400572eb4de80
Asia <asiarie99@gmail.com>
2024-01-31 15:19:34 +0100

    changes of the loop

--- Program.cs
+++ Program.cs
@@ -3,24 +3,34 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
     Console.WriteLine("Let's play Rock-Paper-Scissors!");
-
-    while(true)
+ while(true)
+ {
+    // 1. First Player
+    string? firstPlayerSign;
+    do
     {
-    Console.WriteLine("Provide sign, first player (or write 'quit' to end a game):");
-    string? firstPlayerSign = Console.ReadLine();
+        Console.WriteLine("Provide sign, first player (or write 'quit' to end a game):");
+        firstPlayerSign = Console.ReadLine();
+    } while (firstPlayerSign !="rock" && firstPlayerSign !="paper"&& firstPlayerSign!="scissors" && firstPlayerSign != "quit");
 
-    if (firstPlayerSign == "quit")
+       if (firstPlayerSign == "quit")
+        {
+            break;
+        }
+        //2. Second Player
+    string? secondPlayerSign;
+    do
     {
-        break;
-    }
-
     Console.WriteLine ("Provide sign, second player (or write 'quit' to end a game):");
-    string? secondPlayerSign = Console.ReadLine();
+    secondPlayerSign = Console.ReadLine();
+    } 
+    while (secondPlayerSign !="rock" && secondPlayerSign !="paper" && secondPlayerSign !="scissors" && secondPlayerSign !="quit" );
+
     if (secondPlayerSign == "quit")
     {
         break;
     }
-
+    //3. Draw
     if(firstPlayerSign == secondPlayerSign)
     {
         Console.WriteLine("It,s a draw!");
