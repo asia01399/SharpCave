4e63e6596cf4d0506c3371d86976e294b5dbfbc7
Asia <asiarie99@gmail.com>
2024-02-22 20:16:25 +0100

    nie psuć

--- Program.cs
+++ Program.cs
@@ -1,39 +1,45 @@
-﻿   Console.WriteLine("Let's play Rock-Paper-Scissors!");
-    // Type name;
-    // Type veryLongName = value;
-string[] availableSigns = {"rock", "paper", "scissors" };
+﻿using System.ComponentModel;
+
+Console.WriteLine("Let's play Rock-Paper-Scissors!");
+
+string[] availableSigns = { "rock", "paper", "scissors" };
 const string endGameCommand = "quit";
+int firstPlayerPoints = 0;
+int secondPlayerPoints = 0;
+int expectedRoundNumber = 3;
 
+bool keepPlaying = true;
 
- while(true)
+ while (keepPlaying)
  {
-    // 1. First Player
-    string? firstPlayerSign;
-    do
+      for (int roundNumber = 1; roundNumber <= expectedRoundNumber; roundNumber++)
+     {
+     Console.WriteLine($" Round {roundNumber}");
+
+   string? firstPlayerSign;
+   do
     {
-        Console.WriteLine($"Provide sign, first player (or write '{endGameCommand}' to end a game):");
-        firstPlayerSign = Console.ReadLine();
+    Console.WriteLine($"Provide sign, first player (or write '{endGameCommand}' to end game):");
+    firstPlayerSign = Console.ReadLine().ToLower();
     } while (!availableSigns.Contains(firstPlayerSign) && firstPlayerSign != endGameCommand);
-    
-    // } while (firstPlayerSign !="rock" && firstPlayerSign !="paper"&& firstPlayerSign!="scissors" && firstPlayerSign != endGameCommand);
-    
 
        if (firstPlayerSign == endGameCommand)
         {
+            keepPlaying = false;
             break;
         }
 
-    //2. Second Player
     string? secondPlayerSign;
     do
     {
     Console.WriteLine ($"Provide sign, second player (or write '{endGameCommand}' to end a game):");
     secondPlayerSign = Console.ReadLine();
     } 
-    while (secondPlayerSign !="rock" && secondPlayerSign !="paper" && secondPlayerSign !="scissors" && secondPlayerSign !=endGameCommand );
+    while (!availableSigns.Contains(secondPlayerSign) && secondPlayerSign != endGameCommand);
 
     if (secondPlayerSign == endGameCommand)
     {
+        keepPlaying = false;
         break;
     }
 
@@ -42,7 +48,6 @@ const string endGameCommand = "quit";
     string winningWithSecondPlayerSign = availableSigns[winningWithSecondPlayerSignIndex];
 
 
-    //3. Draw
     if(firstPlayerSign == secondPlayerSign)
     {
         Console.WriteLine("It,s a draw!");
@@ -50,14 +55,33 @@ const string endGameCommand = "quit";
 
     else if(firstPlayerSign == winningWithSecondPlayerSign)
     { 
-        Console.WriteLine("First Player won!");
+        Console.WriteLine($"First Player won: {firstPlayerSign} beats {secondPlayerSign}!");
+        firstPlayerPoints += 1;
     }
     
     else
     {
-        Console.WriteLine("Second Player won!");
+        Console.WriteLine($"Second Player won: {secondPlayerSign} beats {firstPlayerSign}!");
+        secondPlayerPoints += 1;
     }
-    }
+    Console.WriteLine($"Current points: Player1: {firstPlayerPoints} Player2: {secondPlayerPoints}");
+     }
 
+    if (firstPlayerPoints > secondPlayerPoints)
+    { 
+        Console.WriteLine($"First player crushed second player {firstPlayerPoints} to {secondPlayerPoints}!");
+    }
+    else if (secondPlayerPoints > firstPlayerPoints)
+    {
+        Console.WriteLine ($"Second player crushed first player {secondPlayerPoints} to {firstPlayerPoints}!");
+    }
+    else
+    {
+       Console.WriteLine ($"It's a total draw {firstPlayerPoints} to {secondPlayerPoints}");
+}
+
+    firstPlayerPoints = 0;
+    secondPlayerPoints = 0;
+    }
 Console.WriteLine("Press Enter to close the game...");
 Console.ReadLine();
